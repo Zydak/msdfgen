3f32d90b97217ba0d55cce16f1afb7826d0cde9d
Zydak <koksszmoks@gmail.com>
2023-08-26 15:32:24 +0200

    WIndows build

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "freetypeg"
+project "freetype"
 	kind "StaticLib"
 	location "freetype"
 	language "C"
@@ -17,50 +17,8 @@ project "freetypeg"
 		"freetype/include/freetype/config/*.h",
 		"freetype/include/freetype/internal/*.h",
 
-		"freetype/src/autofit/autofit.c",
-		"freetype/src/base/*.c",
-		"freetype/src/bdf/bdf.c",
-		"freetype/src/bzip2/ftbzip2.c",
-		"freetype/src/cache/ftcache.c",
-		"freetype/src/cff/cff.c",
-		"freetype/src/cid/type1cid.c",
-		"freetype/src/gzip/ftgzip.c",
-		"freetype/src/lzw/ftlzw.c",
-		"freetype/src/pcf/pcf.c",
-		"freetype/src/pfr/pfr.c",
-		"freetype/src/psaux/psaux.c",
-		"freetype/src/pshinter/pshinter.c",
-		"freetype/src/psnames/psnames.c",
-		"freetype/src/raster/raster.c",
-		"freetype/src/sdf/sdf.c",
-		"freetype/src/sfnt/sfnt.c",
-		"freetype/src/smooth/smooth.c",
-		"freetype/src/truetype/truetype.c",
-		"freetype/src/type1/type1.c",
-		"freetype/src/type42/type42.c",
-		"freetype/src/winfonts/winfnt.c"
-
 		-- "freetype/src/autofit/autofit.c",
-		-- "freetype/src/base/ftbase.c",
-		-- "freetype/src/base/ftbbox.c",
-		-- "freetype/src/base/ftbdf.c",
-		-- "freetype/src/base/ftbitmap.c",
-		-- "freetype/src/base/ftcid.c",
-		-- "freetype/src/base/ftdebug.c",
-		-- "freetype/src/base/ftfstype.c",
-		-- "freetype/src/base/ftgasp.c",
-		-- "freetype/src/base/ftglyph.c",
-		-- "freetype/src/base/ftgxval.c",
-		-- "freetype/src/base/ftinit.c",
-		-- "freetype/src/base/ftmm.c",
-		-- "freetype/src/base/ftotval.c",
-		-- "freetype/src/base/ftpatent.c",
-		-- "freetype/src/base/ftpfr.c",
-		-- "freetype/src/base/ftstroke.c",
-		-- "freetype/src/base/ftsynth.c",
-		-- "freetype/src/base/ftsystem.c",
-		-- "freetype/src/base/fttype1.c",
-		-- "freetype/src/base/ftwinfnt.c",
+		-- "freetype/src/base/*.c",
 		-- "freetype/src/bdf/bdf.c",
 		-- "freetype/src/bzip2/ftbzip2.c",
 		-- "freetype/src/cache/ftcache.c",
@@ -81,6 +39,48 @@ project "freetypeg"
 		-- "freetype/src/type1/type1.c",
 		-- "freetype/src/type42/type42.c",
 		-- "freetype/src/winfonts/winfnt.c"
+
+		"freetype/src/autofit/autofit.c",
+		"freetype/src/base/ftbase.c",
+		"freetype/src/base/ftbbox.c",
+		"freetype/src/base/ftbdf.c",
+		"freetype/src/base/ftbitmap.c",
+		"freetype/src/base/ftcid.c",
+		"freetype/src/base/ftdebug.c",
+		"freetype/src/base/ftfstype.c",
+		"freetype/src/base/ftgasp.c",
+		"freetype/src/base/ftglyph.c",
+		"freetype/src/base/ftgxval.c",
+		"freetype/src/base/ftinit.c",
+		"freetype/src/base/ftmm.c",
+		"freetype/src/base/ftotval.c",
+		"freetype/src/base/ftpatent.c",
+		"freetype/src/base/ftpfr.c",
+		"freetype/src/base/ftstroke.c",
+		"freetype/src/base/ftsynth.c",
+		"freetype/src/base/ftsystem.c",
+		"freetype/src/base/fttype1.c",
+		"freetype/src/base/ftwinfnt.c",
+		"freetype/src/bdf/bdf.c",
+		"freetype/src/bzip2/ftbzip2.c",
+		"freetype/src/cache/ftcache.c",
+		"freetype/src/cff/cff.c",
+		"freetype/src/cid/type1cid.c",
+		"freetype/src/gzip/ftgzip.c",
+		"freetype/src/lzw/ftlzw.c",
+		"freetype/src/pcf/pcf.c",
+		"freetype/src/pfr/pfr.c",
+		"freetype/src/psaux/psaux.c",
+		"freetype/src/pshinter/pshinter.c",
+		"freetype/src/psnames/psnames.c",
+		"freetype/src/raster/raster.c",
+		"freetype/src/sdf/sdf.c",
+		"freetype/src/sfnt/sfnt.c",
+		"freetype/src/smooth/smooth.c",
+		"freetype/src/truetype/truetype.c",
+		"freetype/src/type1/type1.c",
+		"freetype/src/type42/type42.c",
+		"freetype/src/winfonts/winfnt.c"
 	}
 
 	includedirs
@@ -118,8 +118,6 @@ project "msdfgen"
 	platforms { "Windows", "Linux" }
 	staticruntime "off"
 
-    dependson "freetypeg"
-
     objdir "build/obj/%{cfg.buildcfg}"
     targetdir "build/bin/%{cfg.buildcfg}"
 
@@ -143,7 +141,7 @@ project "msdfgen"
 
 	links
 	{
-		"freetypeg",
+		"freetype",
 	}
 
 	filter "system:windows"
